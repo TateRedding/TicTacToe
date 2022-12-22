83fbf7a0d72ce4562a87ffed11395857766aa376
Tate Redding <tateredding@gmail.com>
2022-12-22 09:28:52 -0700

    added custom grid size

--- app.js
+++ app.js
@@ -12,11 +12,8 @@ const state = {
             difficulty: "easy"
         }
     ],
-    board: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ],
+    gridSize: 3,
+    board: [],
     activeLetter: 'X'
 };
 
@@ -31,6 +28,12 @@ const vsHumanButton = document.getElementById("vs_human");
 const vsComputerButton = document.getElementById("vs_computer");
 const startGameButton = document.getElementById("start_game");
 
+const customGridDiv = document.getElementById("custom_grid_div");
+const customGridInput = document.getElementById("custom_grid_input");
+const customGridButton = document.getElementById("custom_grid_button");
+
+// Can any of these be combined via class name?
+
 const playerOneNameInput = document.getElementById("player_one_name_input");
 const playerOneNameButton = document.getElementById("player_one_name_button");
 const playerOneNameDisplay = document.getElementById("player_one_name");
@@ -42,10 +45,13 @@ const playerTwoNameDisplay = document.getElementById("player_two_name");
 const playerInfoDivs = document.getElementsByClassName("player_info");
 
 const difficultySelection = document.getElementById("difficulty_selection");
+const gridSizeSelection = document.getElementById("grid_size_selection");
 
 const gameGrid = document.getElementById("game_grid");
 
 // EVENT LISTENERS
+
+// --- SETTING UP THE GAME ---
 vsHumanButton.addEventListener("click", () => {
     switchToSecondScreen();
     computerColumn.style.display = "none";
@@ -64,6 +70,7 @@ playerOneNameButton.addEventListener("click", () => {
     };
     playerOneNameButton.disabled = true;
     playerOneNameInput.disabled = true;
+    state.currentTurn = state.players[0].name;
     enableStartButtonIfReady();
 });
 
@@ -80,20 +87,47 @@ difficultySelection.addEventListener("change", (event) => {
     state.players[1].difficulty = event.target.value;
 });
 
+gridSizeSelection.addEventListener("change", (event) => {
+    console.log(customGridDiv);
+    if (event.target.value === "custom") {
+        customGridDiv.style.visibility = "visible";
+        state.gridSize = 0;
+    } else {
+        customGridDiv.style.visibility = "hidden";
+        state.gridSize = Number(event.target.value);
+    }
+    enableStartButtonIfReady();
+});
+
+customGridButton.addEventListener("click", () => {
+    if (!customGridInput.value) {
+        return;
+    } else if (customGridInput.value > 9) {
+        state.gridSize = 9;
+    } else {
+        state.gridSize = customGridInput.value;
+    };
+    customGridInput.disabled = true;
+    customGridButton.disabled = true;
+    enableStartButtonIfReady();
+});
+
 startGameButton.addEventListener("click", () => {
+    createGrid();
     secondScreen.style.display = "none";
     playerOneNameDisplay.innerText = state.players[0].name;
     playerTwoNameDisplay.innerText = state.players[1].name;
     gameScreen.style.display = "flex";
 });
 
+// --- PLAYING THE GAME ---
+
 gameGrid.addEventListener("click", (event) => {
     if (!event.target.innerText) {
-        const row = Array.from(gameGrid.children[0].children).indexOf(event.target.parentNode);
+        const row = Array.from(gameGrid.children).indexOf(event.target.parentNode);
         const col = Array.from(event.target.parentNode.children).indexOf(event.target);
         event.target.innerText = state.activeLetter;
         state.board[row][col] = state.activeLetter;
-        console.log(state.board);
         if (state.players[1].isHuman) {
             switchPlayers();
         }
@@ -107,7 +141,7 @@ const switchToSecondScreen = () => {
 };
 
 const enableStartButtonIfReady = () => {
-    if (state.players[0].name && state.players[1].name) {
+    if (state.players[0].name && state.players[1].name && state.gridSize) {
         startGameButton.disabled = false;
     };
 };
@@ -116,7 +150,43 @@ const switchPlayers = () => {
     console.log("switch")
     for (let i = 0; i < state.players.length; i++) {
         state.players[i].onTurn = !state.players[i].onTurn;
-        state.players[i].onTurn ? playerInfoDivs[i].style.border = "1px solid yellow" : playerInfoDivs[i].style.border = "none";
+        state.players[i].onTurn ? playerInfoDivs[i].classList.add("current_turn_display"): playerInfoDivs[i].classList.remove("current_turn_display");
     }
     state.activeLetter === 'X' ? state.activeLetter = 'O' : state.activeLetter = 'X';
 };
+
+const createGrid = () => {
+    for (let i = 0; i < state.gridSize; i++) {
+        const newRow = document.createElement("tr");
+        const newRowArr = [];
+        for (let i = 0; i < state.gridSize; i++) {
+            const newCell = document.createElement("td");
+            newRow.appendChild(newCell);
+            newRowArr.push(null);
+        }
+        gameGrid.appendChild(newRow);
+        state.board.push(newRowArr);
+    };
+};
+
+// reset state that initialisez the gamestate like demo from 12-22
+
+// style.display = "none" vs .remove() vs adding hidden attribute (it wont move things)
+
+// maybe a currentTurn in state instead of the onTurn for the players
+
+//isOnDiagonal
+//win checker
+
+
+// AI
+//get all possible moves
+//get all winning moves
+
+// easy looks for a win, then picks at random
+
+//medium will look for a win, then try to block me, then move at random
+
+// hard will look for a win, then try to block, then try to make best move to put itself in a winning position (something that puts a second O in a row/ only one more move to win)
+
+// extra hard, which i wont do, wuld look several steps ahead, trying to strategize best way to create a position of two possible win moves in one turn
