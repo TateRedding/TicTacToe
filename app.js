cbafebb1606a1d21d993dcdbcb23d425b9b7ca02
Tate Redding <tateredding@gmail.com>
2022-12-22 12:51:41 -0700

    added functionality for winning or tieing a game

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const state = {
     players: [
         {
             name: "",
-            onTurn: true
+            onTurn: true,
         },
         {
             name: "",
@@ -14,7 +14,9 @@ const state = {
     ],
     gridSize: 3,
     board: [],
-    activeLetter: 'X'
+    currentTurn: '',
+    activeLetter: 'X',
+    gameOver: false
 };
 
 // DOM SELCTORS
@@ -31,6 +33,8 @@ const startGameButton = document.getElementById("start_game");
 const customGridDiv = document.getElementById("custom_grid_div");
 const customGridInput = document.getElementById("custom_grid_input");
 const customGridButton = document.getElementById("custom_grid_button");
+
+const gameStatusDisplay = document.getElementById("game_status");
 
 // Can any of these be combined via class name?
 
@@ -65,9 +69,10 @@ vsComputerButton.addEventListener("click", () => {
 });
 
 playerOneNameButton.addEventListener("click", () => {
-    if (playerOneNameInput.value) {
-        state.players[0].name = playerOneNameInput.value;
+    if (!playerOneNameInput.value) {
+        return;
     };
+    state.players[0].name = playerOneNameInput.value;
     playerOneNameButton.disabled = true;
     playerOneNameInput.disabled = true;
     state.currentTurn = state.players[0].name;
@@ -75,9 +80,10 @@ playerOneNameButton.addEventListener("click", () => {
 });
 
 playerTwoNameButton.addEventListener("click", () => {
-    if (playerTwoNameInput.value) {
-        state.players[1].name = playerTwoNameInput.value;
+    if (!playerTwoNameInput.value) {
+        return;
     };
+    state.players[1].name = playerTwoNameInput.value;
     playerTwoNameButton.disabled = true;
     playerTwoNameInput.disabled = true;
     enableStartButtonIfReady();
@@ -88,14 +94,13 @@ difficultySelection.addEventListener("change", (event) => {
 });
 
 gridSizeSelection.addEventListener("change", (event) => {
-    console.log(customGridDiv);
     if (event.target.value === "custom") {
         customGridDiv.style.visibility = "visible";
         state.gridSize = 0;
     } else {
         customGridDiv.style.visibility = "hidden";
         state.gridSize = Number(event.target.value);
-    }
+    };
     enableStartButtonIfReady();
 });
 
@@ -117,21 +122,29 @@ startGameButton.addEventListener("click", () => {
     secondScreen.style.display = "none";
     playerOneNameDisplay.innerText = state.players[0].name;
     playerTwoNameDisplay.innerText = state.players[1].name;
+    gameStatusDisplay.innerText = `It's ${state.currentTurn}'s turn!`;
     gameScreen.style.display = "flex";
 });
 
 // --- PLAYING THE GAME ---
 
 gameGrid.addEventListener("click", (event) => {
+    if (state.gameOver) {
+        return;
+    };
     if (!event.target.innerText) {
         const row = Array.from(gameGrid.children).indexOf(event.target.parentNode);
         const col = Array.from(event.target.parentNode.children).indexOf(event.target);
         event.target.innerText = state.activeLetter;
         state.board[row][col] = state.activeLetter;
-        if (state.players[1].isHuman) {
+
+        winCheck();
+        fullBoardCheck();
+
+        if (!state.gameOver) {
             switchPlayers();
-        }
-    }
+        };
+    };
 });
 
 // HELPER FUNCTIONS
@@ -147,12 +160,13 @@ const enableStartButtonIfReady = () => {
 };
 
 const switchPlayers = () => {
-    console.log("switch")
     for (let i = 0; i < state.players.length; i++) {
         state.players[i].onTurn = !state.players[i].onTurn;
         state.players[i].onTurn ? playerInfoDivs[i].classList.add("current_turn_display"): playerInfoDivs[i].classList.remove("current_turn_display");
-    }
+    };
     state.activeLetter === 'X' ? state.activeLetter = 'O' : state.activeLetter = 'X';
+    state.currentTurn === state.players[0].name ? state.currentTurn = state.players[1].name : state.currentTurn = state.players[0].name;
+    gameStatusDisplay.innerText = `It's ${state.currentTurn}'s turn!`;
 };
 
 const createGrid = () => {
@@ -163,23 +177,114 @@ const createGrid = () => {
             const newCell = document.createElement("td");
             newRow.appendChild(newCell);
             newRowArr.push(null);
-        }
+        };
         gameGrid.appendChild(newRow);
         state.board.push(newRowArr);
     };
 };
 
-// reset state that initialisez the gamestate like demo from 12-22
+const winCheck = () => {
+    for (let i = 0; i < state.gridSize; i++) {
+        if (isAllTheSame(getRow(i))) {
+            triggerWin(state.currentTurn, getRow(i));
+            return;
+        };
+        if (isAllTheSame(getColumn(i))) {
+            triggerWin(state.currentTurn, getColumn(i));
+            return;
+        };
+    };
+    if (isAllTheSame(getLtoRDiagonal())) {
+        triggerWin(state.currentTurn, getLtoRDiagonal());
+        return;
+    };
+    if (isAllTheSame(getRtoLDiagonal())) {
+        triggerWin(state.currentTurn, getRtoLDiagonal());
+        return;
+    };
+};
 
-// style.display = "none" vs .remove() vs adding hidden attribute (it wont move things)
+const getRow = (num) => {
+    let result = [];
+    for (let i = 0; i < state.gridSize; i++) {
+        result.push([num, i]);
+    };
+    return result;
+};
 
-// maybe a currentTurn in state instead of the onTurn for the players
+const getColumn = (num) => {
+    let result = [];
+    for (let i = 0; i < state.gridSize; i++) {
+        result.push([i, num]);
+    };
+    return result;
+};
 
-//isOnDiagonal
-//win checker
+const getLtoRDiagonal = () => {
+    let result = [];
+    for (let i = 0; i < state.gridSize; i++) {
+        result.push([i, i]);
+    };
+    return result;
+};
 
+const getRtoLDiagonal = () => {
+    let result = [];
+    for (let i = 0; i < state.gridSize; i++) {
+        result.push([i, state.gridSize-(i+1)]);
+    };
+    return result;
+};
+
+const isAllTheSame = (arr) => {
+    if (state.board[arr[0][0]][arr[0][1]] === null) {
+        return false;
+    };
+    const first = state.board[arr[0][0]][arr[0][1]];
+    for (let i = 1; i < arr.length; i++) {
+        currResult = state.board[arr[i][0]][arr[i][1]];
+        if (currResult === null || currResult !== first) {
+            return false;
+        };
+    };
+    return true;
+};
+
+const triggerWin = (player, arr) => {
+    state.gameOver = true;
+    gameStatusDisplay.innerText = `${state.currentTurn} wins!`
+    for (let i = 0; i < arr.length; i++) {
+        currChild = gameGrid.children[arr[i][0]].children[arr[i][1]];
+        currChild.style.fontWeight = "bold";
+        currChild.style.color = "red";
+    };
+};
+
+const fullBoardCheck = () => {
+    if (state.gameOver) {
+        return;
+    };
+    for (let i = 0; i < state.gridSize; i++) {
+        for (let j = 0; j < state.gridSize; j++) {
+            if (state.board[i][j] === null) {
+                return;
+            };
+        };
+    };
+
+    state.gameOver = true;
+    gameStatusDisplay.innerText = `Game over! No one wins!`;
+
+};
+
+// Is there possibly a simpler soplution to full board check? Maybe keep track of how many times a td has been altered,
+// and check if its ever equal to gridsize squared
+
+// Would there be a better way to format winCheck/ full borardCheck so that they return booleans
+// in an effort for cleaner code in the gamegrid event listener
 
 // AI
+// in grid event listener, should check if player 2 is computer and on turn, if so, return out of function
 //get all possible moves
 //get all winning moves
 
